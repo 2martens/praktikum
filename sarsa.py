f16ed611982a7b2e3ab138a62bdb67c81bde539a
Jim Martens <jim1@live.de>
2014-11-24 16:08:36 +0100

    Fixed algorithm

--- sarsa.py
+++ sarsa.py
@@ -18,7 +18,8 @@ class SARSA_Algorithm(object):
 		aVector,a = grid_world.getAction(h, self.beta)
 		val = np.dot(self.weightTable[a], s)
 		r = world.get_reward()
-		while r == 0:
+		duration = 0
+		while r == False:
 			world.act(aVector.tolist())
 			s_next = world.get_sensor()
 			r = world.get_reward()
@@ -26,8 +27,8 @@ class SARSA_Algorithm(object):
 			aVector, a_next = grid_world.getAction(h, self.beta)
 			val_next = np.dot(self.weightTable[a_next], s_next)
 
-			if  r == 1.0:                                   
-				target = r                                  
+			if  r == True:                                   
+				target = 1.0                                 
 			else:
 				target = 0.9 * val_next
 
@@ -36,11 +37,25 @@ class SARSA_Algorithm(object):
 			s[0:self.map_size] = s_next[0:self.map_size]
 			val = val_next
 			a = a_next
+			duration += 1
+
+		return duration
+
+	def getWeights(self):
+		return self.weightTable
 
 if __name__ == '__main__':
-	size_a, size_b = 16, 16
+	size_a, size_b = 3, 3
 	worldObj = grid_world.world(size=(size_a, size_b))
 	map_size = size_a * size_b
 	weightTable = np.random.uniform (0.0, 0.0, (4, map_size))
-	sarsaObject = SARSA_Algorithm(50, 0.2, weightTable, map_size)
-	sarsaObject.sarsa(worldObj)
+	sarsaObject = SARSA_Algorithm(50, 0.5, weightTable, map_size)
+	d_sum = 0
+	for i in range(0, 5):
+		duration = sarsaObject.sarsa(worldObj)
+		d_sum += duration
+		print('weights: ')
+		print(sarsaObject.getWeights())
+
+	print('Durchschnitt duration:')
+	print(d_sum / 5)
